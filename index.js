a565107322a925d71c9cec43640ac157c437794a
abhinaya014 <abhinayadahal407@gmail.com>
2024-05-01 00:38:17 +0200

    Refactor API routes and fix CORS configuration

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const apiRoutes = require('./api');  // Asegúrate de tener este archivo configurado con las rutas de tu API.
+const apiRoutes = require('./api');
 const cors = require('cors');
 
-const app = express();
+let app = express();
 
-// Configuración de CORS para permitir conexiones desde ciertos orígenes
+// Configuración de CORS con URLs corregidas
 app.use(cors({
     origin: ['http://localhost:8080', 'http://127.0.0.1:8080', 'http://3.222.27.101:8080']
 }));
 
-// Uso de bodyParser para parsear cuerpos de solicitud JSON
+// Middleware para parsear JSON
 app.use(bodyParser.json());
 
 // Conexión a MongoDB
-mongoose.connect('mongodb://localhost/netalmix', { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-});
+mongoose.connect('mongodb://localhost/netalmix', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const db = mongoose.connection;
+// Eventos de conexión
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, "Error connecting to database"));
-db.once('open', function() {
+db.once('open', function callback() {
     console.log("Database connected successfully");
 });
 
-// Define un puerto y asegúrate de que el servidor escuche en todas las interfaces
-const PORT = process.env.PORT || 8080;
-app.get('/info', (req, res) => res.send('Hello from NetAlmix!'));
+// Definición del puerto y corrección en la variable usada
+var port = process.env.PORT || 8080;
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
+// Ruta básica para probar la conexión
+app.get('/info', (req, res) => res.send('Hello World!'));
+
+// Iniciando el servidor en todas las interfaces
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${port}`);
 });
 
-// Uso de rutas de la API
+// Rutas de la API
 app.use('/api', apiRoutes);
-
