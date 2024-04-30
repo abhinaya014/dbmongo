1727cca34cc5530671e9bf81754544e7870f4820
abhinaya014 <abhinayadahal407@gmail.com>
2024-04-30 23:22:40 +0200

    ss

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const apiRoutes = require('./api');
-const cors = require('cors');
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const mongoose = require ('mongoose');
+const apiRoutes = require ('./api');
+const cors = require ('cors');
 
-let app = express();
+let app = express ();
 
-app.use(cors({
-    origin: ['http://localhost:8080', 'http://127.0.0.1:8080', ]
+
+app.use(cors({ origin: ['http://localhost:8080', 'http;//127.0.0.1:8080', 'http://3.222.27.101']
 }));
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/netalmi', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost/netakmi', { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 if (!db) {
-    console.log("ERROR connecting db");
+    console.log("Error connecting db");
 } else {
-    console.log("DB connected successfully");
+    console.log("Db connected successfully");
 }
 
-var port = process.env.port || 8080;
+var port = process.env.PORT || 8080;
+app.get('/info', (req, res) => res.send('Hello World!'));
 
-app.get('/info', (req, res) => res.send('El mejor WS de la historia'));
-
-app.listen(port, function () {
-    console.log("Running on port: " + port);
+app.listen(port, function(){
+    console.log('Running on port ' + port);
 });
 
-app.use('/', apiRoutes);
+app.use('/api', apiRoutes);
