b23e4b49cea3ac68ae71e6511e88d643f42e73bb
abhinaya014 <abhinayadahal407@gmail.com>
2024-04-30 23:53:46 +0200

    sds

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ if (!db) {
 var port = process.env.PORT || 8080;
 app.get('/info', (req, res) => res.send('Hello World!'));
 
-app.listen(port,'8080', function(){
+app.listen(port, function(){
     console.log('Running on port ' + port);
 });
 
