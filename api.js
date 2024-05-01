105740f7a7f06e8f9fd8b5a8e19c829bee4cabd3
abhinaya014 <abhinayadahal407@gmail.com>
2024-05-01 11:32:46 +0200

    sasd

--- api.js
+++ api.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/api', function (req, res) {
+    res.json({
+        status: 'Api trabajando',
+        message: 'Bienvenid@s al mejor WS del mundo'
+    });
+});
+
 
 router.get('/info',(req, res)=>{
     res.send('Hello World!');
