d0a038014516b0f3ac694c1aa4cc690fc0640432
abhinaya014 <abhinayadahal407@gmail.com>
2024-05-02 18:19:02 +0200

    sasd

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const apiRoutes = require('./api');
+const rutasApi = require('./api');
 const cors = require('cors');
 
 let app = express();
 
+// Configuración de CORS para permitir solicitudes desde ciertos orígenes
 app.use(cors({
-    origin: ['http://localhost:8080', 'http://127.0.0.1:8080', 'http://3.222.27.101:8080']
+    origin: ['http://localhost:8080', 'http://127.0.0.1:8080']
 }));
 
-
-app.use(bodyParser.json());
+// Middleware para parsear cuerpos de solicitud JSON
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// Conexión a MongoDB
+mongoose.connect('mongodb://localhost/netalmix', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conexión a MongoDB exitosa'))
+  .catch(err => console.error('Error de conexión a MongoDB:', err));
 
-mongoose.connect('mongodb://localhost/NetAlmix', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const PORT = process.env.PORT || 8080;
 
+// Ruta de información básica
+app.get('/info', (req, res) => res.send('El mejor servicio web de la historia'));
 
-  const PORT = process.env.PORT || 8080;
-
-  app.get('/info', (req, res)=>res.send('El módulo de API está funcionando correctamente'));
-
-
+// Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+    console.log(`Servidor ejecutándose en el puerto: ${PORT}`);
+});
 
-
-app.use('/api', apiRoutes);
-
+// Usar rutas API
+app.use('/api', rutasApi);
