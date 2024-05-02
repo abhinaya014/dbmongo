a9994771c5820b8ba211e1146506b34cbd504896
abhinaya014 <abhinayadahal407@gmail.com>
2024-05-02 18:17:23 +0200

    sds

--- api.js
+++ api.js
@@ -1,21 +1,3 @@
 const express = require('express');
-const router = express.Router();
-const {
-  createContent,
-  deleteContent,
-  updateContent,
-  getAllSeries,
-  getAllMovies,
-  getContentsByGenre,
-  getTopContents
-} = require('./controlador'); // Asegúrate de que la ruta es correcta
-
-router.post('/contents', createContent);
-router.delete('/contents/:id', deleteContent);
-router.put('/contents/:id', updateContent);
-router.get('/series', getAllSeries);
-router.get('/movies', getAllMovies);
-router.get('/contents/genre/:genre', getContentsByGenre);
-router.get('/contents/top', getTopContents);
-
-module.exports = router;
+const contentController = require('./controlador');
+let router = express.Router();
