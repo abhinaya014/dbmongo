9a6fd6ff89c47541b03c644c677587025eda5fcb
abhinaya014 <abhinayadahal407@gmail.com>
2024-05-02 17:14:59 +0200

    sdsd

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const apiRoutes = require('./api');
+const cors = require('cors');
 
-app.get('/info', (req, res) => {
-  res.send('Hello World!');
+let app = express();
+
+app.use(cors({
+    origin: ['http://localhost:8080', 'http://127.0.0.1:8080', 'http://3.222.27.101:8080']
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/playalmi', { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+if (!db) {
+    console.log("ERROR connecting db");
+} else {
+    console.log("DB connected successfully");
+}
+
+var port = process.env.port || 8080;
+
+app.get('/info', (req, res) => res.send('El mejor WS de la historia'));
+
+app.listen(port, function () {
+    console.log("Running on port: " + port);
 });
 
-const PORT = 8080;
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on http://0.0.0.0:${PORT}`);
-});
+app.use('/', apiRoutes);
