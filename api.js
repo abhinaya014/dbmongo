cee76ba8594a2d6657448ba9348070ab7e50815a
abhinaya014 <abhinayadahal407@gmail.com>
2024-05-02 18:22:00 +0200

    asdf

--- api.js
+++ api.js
@@ -1,3 +1,14 @@
 const express = require('express');
-const contentController = require('./controlador');
-let router = express.Router();
+const router = express.Router();
+const controlador = require('./controlador');
+
+// Rutas para manejar el contenido
+router.post('/contenidos', controlador.crearContenido);
+router.delete('/contenidos/:id', controlador.eliminarContenido);
+router.put('/contenidos/:id', controlador.actualizarContenido);
+router.get('/series', controlador.obtenerTodasLasSeries);
+router.get('/peliculas', controlador.obtenerTodasLasPeliculas);
+router.get('/contenidos/genero/:genero', controlador.obtenerContenidoPorGenero);
+router.get('/contenidos/top', controlador.obtenerTopContenidos);
+
+module.exports = router;
