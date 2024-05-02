88cba96225a8c1639e2b6bcf0764097424c41ab0
abhinaya014 <abhinayadahal407@gmail.com>
2024-05-02 18:38:46 +0200

    asas

--- api.js
+++ api.js
@@ -4,11 +4,6 @@ const controlador = require('./controlador');
 
 // Rutas para manejar el contenido
 router.post('/contenidos', controlador.crearContenido);
-router.delete('/contenidos/:id', controlador.eliminarContenido);
-router.put('/contenidos/:id', controlador.actualizarContenido);
-router.get('/series', controlador.obtenerTodasLasSeries);
-router.get('/peliculas', controlador.obtenerTodasLasPeliculas);
-router.get('/contenidos/genero/:genero', controlador.obtenerContenidoPorGenero);
-router.get('/contenidos/top', controlador.obtenerTopContenidos);
+
 
 module.exports = router;
