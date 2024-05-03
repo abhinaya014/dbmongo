4885039e721c46185671f622944babeb7d2885dc
abhinaya014 <abhinayadahal407@gmail.com>
2024-05-03 13:11:37 +0200

    sdsd

--- controlador.js
+++ controlador.js
@@ -94,14 +94,20 @@ exports.creardocumental =async function(req,res) {
 
 exports.eliminarDocumental = async function(req, res) {
   try {
-    const resultado = await Contenido.findByIdAndDelete({ _id: req.params.id, tipoContenido: 'documental' });
-    if (!resultado) return res.status(404).send('Documental no encontrado.');
-    res.status(204).send();
+      const resultado = await Contenido.findByIdAndDelete({
+          _id: req.params.id,
+          tipoContenido: 'documental'
+      });
+
+      if (!resultado) {
+          return res.status(404).send('Documental no encontrado.');
+      }
+
+      res.status(200).json({ message: 'Elemento borrado', resultado });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message });
   }
 };
-
 
 exports.actualizarDocumental = async function(req, res) {
   try {
