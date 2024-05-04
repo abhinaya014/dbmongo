4624f82b4ebf23cf04b69ad48a2ca60c97694c44
abhinaya014 <abhinayadahal407@gmail.com>
2024-05-05 01:18:24 +0200

    dsds

--- api.js
+++ api.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controlador = require('./controlador');
 
-// Rutas para manejar el contenido
 router.post('/contenidos', controlador.crearContenido);
 router.delete('/contenidos/:id', controlador.eliminarContenido);
 router.put('/contenidos/:id', controlador.actualizarContenido);
